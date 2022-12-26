44db26da49c1de12a1b40dc70781d94b03ebcde3
BilShiziheng <shizihenh@163.com>
2022-12-26 17:58:47 +0800

    Site updated: 2022-12-26 17:58:43

--- js/dis.js
+++ js/dis.js
@@ -30,7 +30,7 @@ $.ajax({
         
         function showWelcome() {
         
-            let dist = getDistance(107.38779, 29.70239, ipLoacation.result.location.lng, ipLoacation.result.location.lat);
+            let dist = getDistance(116.23128,40.22077, ipLoacation.result.location.lng, ipLoacation.result.location.lat);
         
             let pos = ipLoacation.result.ad_info.nation;
             let posdesc;
