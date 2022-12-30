ef91464401180126e57b44dd549c137100ac1da3
BilShiziheng <shizihenh@163.com>
2022-12-30 11:54:08 +0800

    Site updated: 2022-12-30 11:54:07

--- js/random.js
+++ js/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/5c54179b/","posts/3ba4c2ca/","posts/bb4ec20c/","posts/53151699/","posts/32dc38c0/","posts/b521e48f/","posts/b3558cea/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
+var posts=["posts/5c54179b/","posts/3ba4c2ca/","posts/bb4ec20c/","posts/53151699/","posts/32dc38c0/","posts/b521e48f/","posts/b3558cea/"];function toRandomPost(){window.open('/'+posts[Math.floor(Math.random() * posts.length)],"_self");};
