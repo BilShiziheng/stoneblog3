3d98f5e6d49b92d6772778aae6b728d514395473
BilShiziheng <shizihenh@163.com>
2023-01-03 08:25:35 +0800

    Site updated: 2023-01-03 08:25:32

--- js/sitetime.js
+++ js/sitetime.js
@@ -12,7 +12,7 @@ function siteTime(){
     var todayHour = today.getHours();
     var todayMinute = today.getMinutes();
     var todaySecond = today.getSeconds();
-    var t1 = Date.UTC(2022,6,4,12,00,00);
+    var t1 = Date.UTC(2022,12,15,12,00,00);
     var t2 = Date.UTC(todayYear,todayMonth,todayDate,todayHour,todayMinute,todaySecond);
     var diff = t2-t1;
     var diffYears = Math.floor(diff/years);
